86296ce33aaeecaa3fd0c28f57eb91cb68a6674b
Dzmitry Liashko <dzliashko@gmail.com>
2025-08-06 14:54:36 +0300

    Add: ch02 ex03

--- ch02/main.go
+++ ch02/main.go
@@ -17,7 +17,20 @@ func ex02() {
 	fmt.Println(f)
 }
 
+func ex03() {
+	var b byte = 255
+	var smallI int32 = 2_147_483_647
+	var bigI uint64 = 18_446_744_073_709_551_615
+	b++
+	smallI++
+	bigI++
+	fmt.Println(b)
+	fmt.Println(smallI)
+	fmt.Println(bigI)
+}
+
 func main() {
 	ex01()
 	ex02()
+	ex03()
 }
