68db4c35d46048f9ca660cb43baf2b605e7b2f31
Dzmitry Liashko <dzliashko@gmail.com>
2025-08-20 09:52:20 +0300

    Add: deferred

--- ch05/main.go
+++ ch05/main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"ch05/closures"
-	"fmt"
-)
+import "ch05/deferred"
 
 func main() {
-	twoBase := closures.MakeMult(2)
-	threeBase := closures.MakeMult(3)
-	for i := 0; i < 3; i++ {
-		fmt.Println(twoBase(i), threeBase(i))
-	}
+	deferred.DeferExample()
 }
